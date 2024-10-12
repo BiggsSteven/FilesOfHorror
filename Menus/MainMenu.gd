e5e8eeed7b18ee1f4849f8723ce5dcfe9e5d24d1
Otirion <Biggs.Steven.M@gmail.com>
2024-10-11 23:08:41 -0400

    Barebones Version of the Main Menu

---
+++ Menus/MainMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_start_button_pressed():
+	print("Start Button Pressed");
+	#get_tree().change_scene("res://Levels/Level_0.tscn");
+
+
+func _on_options_button_pressed():
+	print("Options Button Pressed"); # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	print("Quit Button Pressed");
+	get_tree().quit();
