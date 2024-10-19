746942eefe18d31fcd3bd5a6ea20b3cac5fb403b
Leumasimas <brave.amaterasu@gmail.com>
2024-10-18 17:42:12 -0700

    Corrections to Snapping Mechanic when Flipping

--- scripts/AlingmentDetection.gd
+++ scripts/AlingmentDetection.gd
@@ -3,40 +3,45 @@ extends Area2D
 @onready var AreaNode = $"."
 @onready var ParentNode = $".".get_parent()
 @export var DetectOnFront : bool
-@export var Radius : float
 @export var AngleDetection : float
 
 var newPosition : Vector2
 var newRotation : float
 var activeSnapping = false
+var snapped = false
 var snapChannel = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var circle : CircleShape2D = $CollisionShape2D["shape"]
-	circle.set_radius(Radius)
-	$CollisionShape2D["shape"] = circle
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if !ParentNode["active"] && activeSnapping:
-		ParentNode.global_position = newPosition
-		ParentNode.global_rotation = newRotation
-		GlobalScreenChanger.callSnappingEvent(snapChannel)
-		activeSnapping = false
-
+	if activeSnapping:
+		if !ParentNode["active"] && !snapped:
+			ParentNode.global_position = newPosition
+			ParentNode.global_rotation = newRotation
+			snapped = true
+			GlobalScreenChanger.callSnappingEvent(snapChannel)
+			print("Snapped")
+		if ParentNode["active"] && snapped:
+			snapped = false
 
 func _on_area_entered(area: Area2D) -> void:
-	if ParentNode["active"] && !ParentNode["flipping"] && ParentNode["front"] == DetectOnFront:
-		if deg_to_rad(AngleDetection) >= abs(AreaNode.global_rotation - area.global_rotation):
+	var parentActive = false
+	var parentFlipping = false
+	var parentFront = DetectOnFront
+	if ParentNode["active"] != null: parentActive = ParentNode["active"]
+	if ParentNode["flipping"] != null: parentFlipping = ParentNode["flipping"]
+	if ParentNode["front"] != null: parentFront = ParentNode["front"]
+	if parentActive && !parentFlipping && parentFront == DetectOnFront:
+		print("Snapping Active")
+		if deg_to_rad(AngleDetection)/2 >= abs(AreaNode.global_rotation - area.global_rotation):
 			var posDif : Vector2 = AreaNode.global_position - ParentNode.global_position
 			var angleDif : float = - AreaNode.global_rotation + area.global_rotation
+			if !DetectOnFront: 
+				posDif = -posDif
+				angleDif = angleDif + PI
 			newPosition = area.global_position - posDif.rotated(angleDif)
 			newRotation = ParentNode.global_rotation + angleDif
 			snapChannel = AreaNode.collision_mask
 			activeSnapping = true
-
+			print("Detected Snapping")
 
 func _on_area_exited(area: Area2D) -> void:
 	activeSnapping = false
--- scripts/Paper.gd
+++ scripts/Paper.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-@export var Front : Texture2D
-@export var Back : Texture2D
+@export var FrontTexture : Texture2D
+@export var BackTexture : Texture2D
 @onready var Paper : Area2D = $"."
 @onready var Animation_Tree : AnimationTree = $AnimationTree
 @onready var AreaDetection : CollisionShape2D = $CollisionShape2D
@@ -14,6 +14,7 @@ var active = false
 var flipping = false
 var front = true
 var outside = false
+var newFront = true
 
 var mouse_paper = Vector2(0, 0)
 var paper_mouse_dir = Vector2(0, 0)
@@ -22,25 +23,25 @@ var rotation0 = 0.0
 var rotationMult = 0.0
 
 func _ready() -> void:
-	if Front != null && Back != null:
-		var NewScale : Vector2 = Front.size / FrontSprite.texture.size
+	if FrontTexture != null && BackTexture != null:
+		var NewScale : Vector2 = FrontTexture.size / FrontSprite.texture.size
 		AreaDetection.scale = NewScale
 		ScreenNotifier.scale = NewScale
 		DetectClicking.scale = NewScale
-		FrontSprite.texture = Front
-		BackSprite.texture = Back
-		Back.scale = Front.size / Back.size
+		FrontSprite.texture = FrontTexture
+		BackSprite.texture = BackTexture
+		BackTexture.scale = FrontTexture.size / BackTexture.size
 
 func _process(delta: float) -> void:
 	var mouse_position = get_viewport().get_mouse_position()
 	var new_paper_mouse = mouse_position - Paper.get_global_position()
 	var new_paper_mouse_dir = new_paper_mouse.normalized()
 	var to_rotate = acos(paper_mouse_dir.dot(new_paper_mouse_dir))*sign(paper_mouse_dir.cross(new_paper_mouse_dir))
-	if flipping:
+	if flipping && front != newFront:
 	# Activation of animation of getting flipped
-		Animation_Tree["parameters/conditions/Flip"] = front
-		Animation_Tree["parameters/conditions/Flip Back"] = !front
-		flipping = false
+		Animation_Tree.set("parameters/conditions/Flip",!newFront)
+		Animation_Tree.set("parameters/conditions/Flip Back",newFront)
+		front = newFront
 	if active:	
 	# Generic dragging with mouse
 		mouse_paper = mouse_paper.rotated(to_rotate * rotationMult)
@@ -66,9 +67,9 @@ func _on_texture_button_button_down() -> void:
 		rotationMult = 2 * mouse_paper.length() / size.length()
 		rotationMult = 2 * rotationMult * rotationMult
 		active = true
-	else:
+	elif !flipping && Input.get_mouse_button_mask() == 2:
+		newFront = !front
 		flipping = true
-		front = !front
 
 
 func _on_texture_button_button_up() -> void:
@@ -77,6 +78,9 @@ func _on_texture_button_button_up() -> void:
 	if outside:
 		Paper.global_position = screenCenter
 	active = false
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	flipping = false
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	outside = true
